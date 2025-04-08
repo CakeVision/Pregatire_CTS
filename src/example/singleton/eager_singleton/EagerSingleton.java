7be16605e299fcda241da586a9bf32f0db0c3508
CakeVision <andreicodorean1811@gmail.com>
2025-04-08 21:38:38 +0300

    eager singleton

--- src/example/singleton/eager_singleton/EagerSingleton.java
+++ src/example/singleton/eager_singleton/EagerSingleton.java
@@ -1,4 +1,20 @@
 package example.singleton.eager_singleton;
 
+
 public class EagerSingleton {
+    private static EagerSingleton instance = new EagerSingleton();
+    static {
+        System.out.println("Incarcare clasa de catre JVM");
+        System.out.println("Preluare date de configurare aplicatie");
+    }
+
+    //se executa inainte de fiecare cosntructor
+    {
+        System.out.println("Apel inainte de constructor");
+    }
+
+    private EagerSingleton() {
+        //preluare date conexiune din fisiere, servicii Web, etc
+        System.out.println("Creare conexiune");
+    }
 }
